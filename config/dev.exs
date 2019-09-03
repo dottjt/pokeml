0a68117f78dcee72a1bcce01f50f091ad83b6518
Julius Reade <julius.reade@fmclarity.com>
2019-09-03 10:58:56 +1000

    further configuration

--- config/dev.exs
+++ config/dev.exs
@@ -16,7 +16,7 @@ config :mlp, Mlp.Repo,
 # watchers to your application. For example, we use it
 # with webpack to recompile .js and .css sources.
 config :mlp, MlpWeb.Endpoint,
-  http: [port: 4006],
+  http: [port: 4004],
   debug_errors: true,
   code_reloader: true,
   check_origin: false,
